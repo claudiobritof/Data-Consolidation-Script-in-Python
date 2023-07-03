a102b2a601a0763e6121edf2ca175012e250b7e7
Cláudio Brito <cabf.brito@gmail.com>
2023-07-03 17:57:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -27,3 +27,6 @@ table_exists = mycursor.fetchone()
 
 if table_exists:
     mycursor.execute(f"DROP TABLE {consolidated_raw_data}")
+
+mycursor.execute(f"CREATE TABLE {consolidated_raw_data} (mes VARCHAR(7), rake DOUBLE, jogadores INT, rake_cash_game DOUBLE, rake_torneio DOUBLE, jogadores_cash_game INT, jogadores_torneio INT)")
+
